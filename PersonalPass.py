3dc33ccd18cb54bf5d8bfdf654e2730878a947e5
Sergey Utkin <utkins01@gmail.com>
2016-10-28 11:02:56 +0300

    Initial local commit Signed-off-by: Sergey Utkin <utkins01@gmail.com>

---
+++ PersonalPass.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+# -*- coding: utf8 -*-
+
+# Sergey <DerNitro> Utkin   mail_to: utkins01@gmail.com
+#
+#                    GNU GENERAL PUBLIC LICENSE
+#                       Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+# http://www.gnu.org/licenses/gpl-3.0.txt
+#
+
